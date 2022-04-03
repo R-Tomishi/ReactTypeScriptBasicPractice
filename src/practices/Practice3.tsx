095fbe8be325baaa59c8d4c446ce01a3a5a50d2e
R-Tomishi <43924379+R-Tomishi@users.noreply.github.com>
2022-04-03 16:32:06 +0900

    1

--- src/practices/Practice3.tsx
+++ src/practices/Practice3.tsx
@@ -5,13 +5,15 @@ export const Practice3 = () => {
   };
 
   const onClickPractice = () => {
-    console.log(GetTotalFee(1000));
+    let total: number = 0;
+    total = GetTotalFee(1000);
+    console.log(total);
   };
 
   return (
     <div>
-      <p>練習問題:返却値の型指定</p>
-      <button onClick={onClickPractice}>練習問題2を実行</button>
+      <p>練習問題:変数の型指定</p>
+      <button onClick={onClickPractice}>練習問題3を実行</button>
     </div>
   );
 };
